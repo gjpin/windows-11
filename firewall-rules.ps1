f66a9d088e98a804c566370499eaf030be8bd8b4
gjpin <3874515+gjpin@users.noreply.github.com>
2024-01-03 18:12:16 +0000

    fw: UEFN - FAB

--- firewall-rules.ps1
+++ firewall-rules.ps1
@@ -288,6 +288,10 @@ New-NetFirewallRule -DisplayName "UEFN - Unreal Revision control" -Group "User A
     -Program "%PROGRAMFILES%\Epic Games\Fortnite\FortniteGame\Binaries\Win64\urc.exe" `
     -Enabled True -Action Allow -Direction Outbound -PolicyStore "$env:COMPUTERNAME"
 
+New-NetFirewallRule -DisplayName "UEFN - FAB" -Group "User Applications" `
+    -Program "%PROGRAMFILES%\Epic Games\fortnite\engine\binaries\win64\epicwebhelper.exe" `
+    -Enabled True -Action Allow -Direction Outbound -PolicyStore "$env:COMPUTERNAME"
+
 # Joplin
 New-NetFirewallRule -DisplayName "Joplin" -Group "User Applications" `
     -Program "$env:USERPROFILE\AppData\Local\Programs\Joplin\Joplin.exe" `
