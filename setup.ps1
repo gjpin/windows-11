64836127606a65843e95bbb8e3f27bde2a810280
gjpin <3874515+gjpin@users.noreply.github.com>
2024-07-28 20:37:37 +0100

    minor update

--- setup.ps1
+++ setup.ps1
@@ -194,6 +194,8 @@ Remove-Item -Force "$env:USERPROFILE\OneDrive"
 
 [Environment]::SetEnvironmentVariable('POWERSHELL_TELEMETRY_OPTOUT', 'true', 'Machine')
 
+# Install packages in a non-admin powershell
+
 winget install -e --source winget --id Microsoft.PowerShell
 winget install -e --source winget --id Microsoft.VCRedist.2015+.x64
 winget install -e --source winget --id Microsoft.VCRedist.2013.x64
@@ -220,7 +222,6 @@ winget install -e --source winget --id VirtualDesktop.Streamer
 winget install -e --source winget --id SideQuestVR.SideQuest
 # Download ADB drivers: https://developer.oculus.com/downloads/package/oculus-adb-drivers/
 
-# Install in a non-admin powershell
 winget install -e --source winget --id Spotify.Spotify
 
 ################################################
@@ -291,8 +292,9 @@ dotnet dev-certs https --trust
 winget install -e --source winget --id GoLang.Go
 
 # Install Podman
-winget install -e --source winget --id RedHat.Podman # or Docker: Docker.DockerDesktop
-winget install -e --source winget --id RedHat.Podman-Desktop
+winget install -e --source winget --id Docker.DockerDesktop
+# winget install -e --source winget --id RedHat.Podman
+# winget install -e --source winget --id RedHat.Podman-Desktop
 
 # Install Kubernetes CLIs
 winget install -e --source winget --id Kubernetes.kubectl
