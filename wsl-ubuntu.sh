4b43b2522746b8a37a8c1dd8ab3d2efaf3e85111
gjpin <3874515+gjpin@users.noreply.github.com>
2025-02-03 19:15:26 +0000

    ubuntu wsl: add docker and ssh agent instructions

--- wsl-ubuntu.sh
+++ wsl-ubuntu.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+# SSH Agent
+# eval $(ssh-agent)
+# ssh-add
+
 # Create ssh directory
 mkdir -p $HOME/.ssh
 chmod 700 $HOME/.ssh
@@ -24,3 +28,28 @@ source /home/zero/.bashrc
 
 # Install latest Node LTS
 fnm install --lts
+
+################################################
+##### Docker
+################################################
+
+# References:
+# https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+
+# Install dependencies
+sudo apt install -y ca-certificates curl
+
+# Configure keyring
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+
+# Install Docker
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
